13cc4416f943a90248659670f4002edf4b5f9648
ArkadiptaGhosh <arkadiptaghosh99@gmail.com>
2026-07-04 16:15:46 +0530

    editing weather_client.py

--- src/weather_analytics/api/weather_client.py
+++ src/weather_analytics/api/weather_client.py
@@ -9,6 +9,17 @@ class WeatherClient:
          self.base_url = "https://api.open-meteo.com/v1/forecast"
          self.timeout = 30
     def get_current_weather(self):
+
+        params = {
+            "latitude" : 22.57,
+            "longitude":88.36,
+            "current": "temperature_2m"
+        }
          
         """Retrieve the current weather.""" 
-        response = requests.Response(self.base_url)
+        response = requests.Response(self.base_url,
+                                     params = params)
+        
+        if requests.status_codes == 200 :
+            print("Request successful!")
+            return response.json()
