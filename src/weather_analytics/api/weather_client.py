9593a9023a3e373cab417029dd90a849844e4645
ArkadiptaGhosh <arkadiptaghosh99@gmail.com>
2026-07-04 17:18:12 +0530

    successfully written API to Json

--- src/weather_analytics/api/weather_client.py
+++ src/weather_analytics/api/weather_client.py
@@ -2,23 +2,30 @@ import requests
 
 
 class WeatherClient:
-    "Client for communicating with the Weather API."
-    def __int__(self):
-         """Initialize the Weather API client."""
+    """Client for communicating with the Weather API."""
 
-         self.base_url = "https://api.open-meteo.com/v1/forecast"
-         self.timeout = 30
+    def __init__(self):
+        """Initialize the Weather Client."""
+
+        self.base_url = "https://api.open-meteo.com/v1/forecast"
+
     def get_current_weather(self):
+        """Fetch the current weather."""
 
         params = {
-            "latitude" : 22.57,
-            "longitude":88.36,
+            "latitude": 22.57,
+            "longitude": 88.36,
             "current": "temperature_2m"
         }
-         
-        """Retrieve the current weather.""" 
-        response = requests.Response(self.base_url,
-                                     params = params)
-        
-        if requests.status_codes == 200 :
+
+        response = requests.get(
+            self.base_url,
+            params=params
+        )
+
+        if response.status_code == 200:
             return response.json()
+
+        raise Exception(
+            f"Weather API request failed with status code {response.status_code}"
+        )
