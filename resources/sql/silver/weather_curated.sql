10901e3e0a9be2cdd5f683c6b90b23e1ce47d354
ArkadiptaGhosh <arkadiptaghosh99@gmail.com>
2026-07-15 00:51:14 +0530

    added silver DDL

---
+++ resources/sql/silver/weather_curated.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS weather_analytics.silver.weather_curated (
+    city STRING,
+    latitude DOUBLE,
+    longitude DOUBLE,
+    temperature DOUBLE,
+    humidity INT,
+    wind_speed DOUBLE,
+    weather_time TIMESTAMP,
+    timezone STRING,
+    ingestion_timestamp TIMESTAMP,
+    ingestion_source STRING
+)
+USING DELTA;
